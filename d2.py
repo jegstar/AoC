aaf331472d116048a437c581005b91de26365981
jegstar <wjeggo@hotmail.com>
2022-12-05 07:27:54 +0800

    d2p1

--- d2.py
+++ d2.py
@@ -1,25 +1,33 @@
-input = '''A Y
-B X
-C Z'''
+# input = '''A Y
+# B X
+# C Z'''
 
+with open('d2.txt') as f:
+    input = f.read()
 
 data = [x.split() for x in input.split('\n')]
 
 score = 0
 for i in range(len(data)):
-    if ord(data[i][0]) == ord(data[i][1])-24:
+    a = data[i][0]
+    b = data[i][1]
+    if (a == 'A' and b == 'X' ) or (a == 'B' and b == 'Y' ) or (a == 'C' and b == 'Z' ):
         score += 3
     elif (data[i][0] == 'A' and data[i][1] == 'Y') or (data[i][0] == 'B' and data[i][1] == 'Z') or (data[i][0] == 'C' and data[i][1] == 'X'):
-        score += 0
-    else:
         score += 6
-    
-    let = data[i][0]
-
-    score += ord(let)-64
+    else:
+        score += 0
 
     
+    if b == 'X':
+        score += 1
+    if b == 'Y':
+        score += 2
+    if b == 'Z':
+        score += 3
+
     print(score)
+    
 
 
 
